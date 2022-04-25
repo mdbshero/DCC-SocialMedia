25261420c82f167cf22aeaa5ebbec9c319e7124e
ParkerHadley <parker.l.hadley@gmail.com>
2022-04-25 19:55:31 -0400

    Added style to home,profile,friends,posts buttons

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,10 +1,27 @@
 import React from "react";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
+import "./HomePage.css";
 
 const HomePage = () => {
   const { user } = useContext(AuthContext);
-  return <h1 className="container">Home Page for {user.name}!</h1>;
+  return <h1 className="container">Home Page for {user.name}!
+  <body>
+    <nav class="navbar">
+        <div class="max-width">
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Friends</a></li>
+                <li><a href="#">Posts</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+    </body>
+    </h1>;
 };
 
 export default HomePage;

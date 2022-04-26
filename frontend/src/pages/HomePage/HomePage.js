fca502210434b07ed93b1d288541f86131ce0890
ParkerHadley <parker.l.hadley@gmail.com>
2022-04-25 22:02:23 -0400

    Added form and button to homepage, working on changing it from a search to a post

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -3,25 +3,47 @@ import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
 import "./HomePage.css";
 
-const HomePage = () => {
-  const { user } = useContext(AuthContext);
-  return <h1 className="container">Home Page for {user.name}!
-  <body>
-    <nav class="navbar">
-        <div class="max-width">
-            <ul class="menu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Friends</a></li>
-                <li><a href="#">Posts</a></li>
-            </ul>
-            <div class="menu-btn">
-                <i class="fas fa-bars"></i>
-            </div>
-        </div>
-    </nav>
-    </body>
-    </h1>;
-};
 
-export default HomePage;
+export default class FormSubmission extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchQuery: ""
+    }
+  }
+
+  handleInputChanged(event) {
+    this.setState({
+      searchQuery: event.target.value
+    });
+  }
+
+  handleButtonClicked() {
+    let searchQuery = this.state.searchQuery;
+
+    window.location.href = "https://youtube.com/results?search_query" + searchQuery;
+  }
+
+  render() {
+    return  (
+      <div>
+         <body>
+            <nav class="navbar">
+                <div class="max-width">
+                    <ul class="menu">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Profile</a></li>
+                        <li><a href="#">Friends</a></li>
+                        <li><a href="#">Posts</a></li>
+                    </ul>
+                </div>
+            </nav>
+         </body>
+          <label for="example">What's on your mind?</label>
+            <input type="text" value={this.state.searchQuery} onChange={this.handleInputChanged.bind(this)}/>
+          <button onClick={this.handleButtonClicked.bind(this)}>Submit</button>
+      </div>
+    );
+  }
+}

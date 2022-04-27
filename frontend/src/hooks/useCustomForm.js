b00eae1163b19d270f1a2a6070c60b06f0753054
Mdbshero <mdbshero@gmail.com>
2022-04-26 21:01:19 -0500

    Added Frontend image upload.

--- frontend/src/hooks/useCustomForm.js
+++ frontend/src/hooks/useCustomForm.js
@@ -6,8 +6,13 @@ const useCustomForm = (initialValues = {}, onSubmit) => {
   const handleInputChange = (e) => {
     e.persist();
     if (e.target.name === "isAdmin") {
+      console.log("ADMIN!");
       setFormValues({ ...formData, [e.target.name]: e.target.checked });
+    } else if (e.target.name === "image") {
+      console.log("image!", e.target.files);
+      setFormValues({ ...formData, "image": e.target.files[0] });
     } else {
+      console.log("ELSE!");
       setFormValues({ ...formData, [e.target.name]: e.target.value });
     }
   };
@@ -15,6 +20,7 @@ const useCustomForm = (initialValues = {}, onSubmit) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit(formData);
+    console.log(formData);
   };
 
   const reset = () => {
